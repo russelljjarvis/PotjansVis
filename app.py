b32939005cbf7feefa682b686325a71455dd5933
Russell Jarvis <russelljjarvis@protonmail.com>
2022-04-14 17:08:40 +1000

    update

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ import elephant
 import streamlit.components.v1 as components
 import matplotlib.pyplot as plt
 import networkx as nx
-import pyNN.nest as sim
+import pyNN.neuron as sim
 sim.setup()
 sim.end()
 def nx_chunk(graph, chunk_size):
